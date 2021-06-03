7c2294a3173af3a135a2bfc7f589fed200845e73
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-06-03 18:51:24 +0300

    @/ add index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import Templator from '../templator/Templator';
+
+const tpl = `
+<div
+  class={{wrapper}}
+>
+  {{name}}
+  <div>
+    {{name}}
+  </div>
+</div>
+`;
+
+const div = new Templator(tpl);
+console.log(div.compile({ name: 'Саша' }));
+
