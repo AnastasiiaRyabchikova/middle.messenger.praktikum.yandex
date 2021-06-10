9871dde44d08ebaab2cd902332cd8341943994bd
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-06-10 15:29:16 +0300

    @/index: remove useless code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-// import Templator from '../templator/Templator';
 import Button from './components/Button';
 import render from '../templator/render';
 
-const tpl = `
-<div
-  className={{wrapper}}
->
-  {{name}}
-  <div>
-    {{name}}
-  </div>
-</div>
-`;
-
-// const div = new Templator(tpl);
-// console.log(div.compile({ name: 'Саша' }));
-
-render(document.getElementById('root'));
-console.log(Button.compile({ type: 'button' }));
+render(document.getElementById('root'), Button({}));
 
