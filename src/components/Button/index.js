410579ac367b10068c833b50a23580860dfd8838
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-06-10 13:01:55 +0300

    @/components/Button: fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,6 +2,8 @@ import template from './index.tpl';
 import styles from './styles.module.css';
 import Templator from '../../../templator/Templator';
 
-const componentWithProps = 
+const componentWithProps = {
+  className: `${styles.button} `
+}
 
 export default new Templator(template);
