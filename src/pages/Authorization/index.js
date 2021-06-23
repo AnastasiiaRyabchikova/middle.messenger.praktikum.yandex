df11158b21f015a2b514f9e148c874c5c571358e
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-06-23 10:03:42 +0300

    @/pages/Authorization: add props

--- src/pages/Authorization/index.js
+++ src/pages/Authorization/index.js
@@ -1,8 +1,19 @@
-import template from './index.tpl';
 import Templator from '~/templator';
 
+import Logo from '../../components/Logo';
+
+import template from './index.tpl';
+
+const component = {
+  name: 'AuthorizationPage',
+  template,
+  components: {
+    Logo,
+  },
+};
+
 const Page = (ctx) => {
-  return new Templator(template).compile();
+  return new Templator(component).compile(ctx);
 };
 
 export default Page;
