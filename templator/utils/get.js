ecc99501230be686478474903441c0d4b0bb5e54
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-03 19:58:37 +0300

    templator/utils: fix get

--- templator/utils/get.js
+++ templator/utils/get.js
@@ -1,12 +1,17 @@
 import isObject from './isObject.js';
 
 export default (object, path) => {
-  const keys = path.split('.');
+  const keys = path
+    .split(/[.\[\]]/)
+    .map((item) => item.trim())
+    .filter((item) => item);
   let value = object;
   for (key of keys) {
+    
     if (!isObject(value)) {
       return;
     }
+
     value = value[key];
   }
   return value
