df96c05c374ecc1e42addaef746d23510d802983
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-03 17:23:11 +0300

    @/components/Button: fix styles

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,12 @@
 import template from './index.tpl';
 import styles from './styles.module.css';
 import Templator from '~/templator';
+import cx from '../../utils/classnames';
 
 const Button = (ctx) => {
+
   const props = {
-    className: `${styles.button} ${ctx.className || ''}`,
+    className: cx([styles.button, ctx.className]),
     type: ctx.type || 'button',
     label: ctx.label,
   };
