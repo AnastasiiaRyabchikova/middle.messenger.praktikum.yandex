74b6e976be3c16e3f3ff0acae5723690ceb310cc
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-03 17:40:35 +0300

    @/pages/Acquaintance: add Acquaintance page

---
+++ src/pages/Acquaintance/index.js
@@ -0,0 +1,23 @@
+import Templator from '~/templator';
+
+import Logo from '../../components/Logo';
+import UIInput from '../../components/UIInput';
+import Button from '../../components/Button';
+
+import template from './index.tpl';
+
+const component = {
+  name: 'AuthorizationPage',
+  template,
+  components: {
+    Logo,
+    UIInput,
+    Button,
+  },
+};
+
+const Page = (ctx) => {
+  return new Templator(component).compile(ctx);
+};
+
+export default Page;
---
+++ src/pages/Acquaintance/index.tpl.js
@@ -0,0 +1,45 @@
+import styles from './styles.module.css';
+
+const template = `
+<div
+  className="${styles.page}"
+>
+  <header>
+    <Logo
+      className="{{className}}"
+    />
+  </header>
+  <main
+    className="${styles.main}"
+  >
+    <h1
+      className="title title_h1 ${styles.title}"
+    >
+      Знакомство
+    </h1>
+    <form>
+      <UIInput
+        label="Логин"
+        placeholder="Ваш логин"
+      />
+      <UIInput
+        label="Пароль"
+        placeholder="Ваш пароль"
+      />
+      <Button
+        type="submit"
+        className="${styles.submit}"
+        label="Войти"
+      />
+      <a
+        href="#"
+        className="${styles.join}"
+      >
+        Присоединиться
+      </a>
+    </form>
+  </main>
+</div>
+`;
+
+export default template;
