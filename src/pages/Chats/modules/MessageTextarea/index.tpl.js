226ecc2922da4d9bbaff515ca7fffdc162fe4bb7
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-09 09:15:09 +0300

    @/pages/Chats/modules/MessageTextarea: add component for message input

---
+++ src/pages/Chats/modules/MessageTextarea/index.tpl.js
@@ -0,0 +1,18 @@
+import styles from './styles.module.css';
+
+const template = `
+<div
+  class="${styles.wrapper}"
+>
+  <textarea
+    class="${styles.textarea}"
+  />
+  <button
+    type="button"
+  >
+    <IconArrow />
+  </button>
+</div>
+`;
+
+export default template;
