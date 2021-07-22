05bac307d876fddca375ed04df9473e5aa422ef0
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-07-22 11:00:28 +0300

    @/constants/pathnames: add enums

--- src/constants/pathnames.ts
+++ src/constants/pathnames.ts
@@ -1,8 +1,10 @@
-export default {
-  signIn: 'sign-in',
-  signUp: '',
-  changePassword: 'change-password',
-  chats: 'chats',
-  userForm: 'user-form',
-  error500: 'error-500',
+enum pathnames {
+  signIn = 'sign-in',
+  signUp = '',
+  changePassword = 'change-password',
+  chats = 'chats',
+  userForm = 'user-form',
+  error500 = 'error-500',
 };
+
+export default pathnames;
