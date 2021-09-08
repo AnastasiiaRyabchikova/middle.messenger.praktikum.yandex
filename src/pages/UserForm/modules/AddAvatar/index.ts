0a78f263330c8d45a1623e5fbf896105b5076862
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-08 17:42:34 +0300

    @/pages/UserForm/modules/AddAvatar: change containerTemplate

--- src/pages/UserForm/modules/AddAvatar/index.ts
+++ src/pages/UserForm/modules/AddAvatar/index.ts
@@ -22,7 +22,7 @@ export default class Component extends Ryabact.Component {
       components: {
         Avatar,
       },
-      containerTemplate: `<span />`,
+      containerTemplate: `<div />`,
     });
   }
 };
