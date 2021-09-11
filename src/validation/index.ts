0f607ca0c34c973094059d50ebf8fc5ee347c39d
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-11 16:02:29 +0300

    @/validation: add fields validation

---
+++ src/validation/index.ts
@@ -0,0 +1,5 @@
+export { default as name } from './name';
+export { default as login } from './login';
+export { default as email } from './email';
+export { default as phone } from './phone';
+export { default as password } from './password';
---
+++ src/validation/password.ts
@@ -0,0 +1,16 @@
+export default function nameValidation(string: string): string {
+  if (string.length < 8 || string.length > 40) {
+    return 'Значение должно быть длиной от 8 до 40 символов';
+  }
+
+  if (string.toLowerCase() === string) {
+    return 'Значение должно содержать хотя бы одну заглавную букву';
+  }
+
+
+  if (!/[0-9]/.test(string)) {
+    return 'Значение должно содержать хотя бы одну цифру';
+  }
+
+  return '';
+};
