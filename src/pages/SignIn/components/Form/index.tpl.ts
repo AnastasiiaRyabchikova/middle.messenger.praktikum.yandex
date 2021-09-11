647577883af5fa74f3a57d227836601fe44d80ac
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-11 16:06:36 +0300

    @/pages/SignIn/components/Form: add

--- src/pages/SignIn/components/Form/index.tpl.ts
+++ src/pages/SignIn/components/Form/index.tpl.ts
@@ -3,16 +3,93 @@ import * as styles from './styles.module.css';
 const template: string = `
 <form
   name="{{name}}"
+  novalidate=""
 >
-  <t-for={{input of inputs}}>
-    <UIInput
-      class="${styles.input}"
-      appearance="solid"
-      name="{{input.name}}"
-      label="{{input.label}}"
-      placeholder="{{input.placeholder}}"
-    />
-  </t-for>
+  <UIInput
+    class="${styles.input}"
+    appearance="solid"
+    name="email"
+    label="Почта"
+    placeholder="Ваша почта"
+    required="{{required.email}}"
+    error="{{errors.email}}"
+    value="{{params.email}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
+  <UIInput
+    class="${styles.input}"
+    appearance="solid"
+    name="login"
+    label="Логин"
+    placeholder="Ваш логин"
+    required="{{required.login}}"
+    error="{{errors.login}}"
+    value="{{params.login}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
+  <UIInput
+    class="${styles.input}"
+    appearance="solid"
+    name="first_name"
+    label="Имя"
+    placeholder="Ваше имя"
+    required="{{required.first_name}}"
+    error="{{errors.first_name}}"
+    value="{{params.first_name}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
+  <UIInput
+    class="${styles.input}"
+    appearance="solid"
+    name="second_name"
+    label="Фамилия"
+    placeholder="Ваша фамилия"
+    required="{{required.second_name}}"
+    error="{{errors.second_name}}"
+    value="{{params.second_name}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
+  <UIInput
+    type="tel"
+    class="${styles.input}"
+    appearance="solid"
+    name="phone"
+    label="Номер телефона"
+    placeholder="+7( ___ ) ___-__-__"
+    required="{{required.phone}}"
+    error="{{errors.phone}}"
+    value="{{params.phone}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
+  <UIInput
+    class="${styles.input}"
+    appearance="solid"
+    name="password"
+    label="Пароль"
+    placeholder="************"
+    required="{{required.password}}"
+    error="{{errors.password}}"
+    value="{{params.password}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
+  <UIInput
+    class="${styles.input}"
+    appearance="solid"
+    name="password_repeat"
+    label="Повторите пароль"
+    placeholder="************"
+    required="{{required.password_repeat}}"
+    error="{{errors.password_repeat}}"
+    value="{{params.password_repeat}}"
+    evBlur="{{handleInputBlur}}"
+    evInput="{{handleParamsInput}}"
+  />
   <Button
     type="submit"
     class="${styles.submit}"
--- src/pages/SignIn/components/Form/index.ts
+++ src/pages/SignIn/components/Form/index.ts
@@ -1,61 +1,114 @@
 import * as Ryabact from 'ryabact';
+import * as validation from '~/src/validation';
+import isEqual from '~/src/utils/is-equal';
 import { PropsType } from '~/src/types/component';
 import UIInput from '~/src/components/UIInput';
 import Button from '~/src/components/Button';
 import template from './index.tpl';
 
-const inputs = [
-  {
-    id: 'email',
-    name: 'email',
-    label: 'Почта',
-    placeholder: 'Ваша почта',
-  },
-  {
-    id: 'login',
-    name: 'login',
-    label: 'Логин',
-    placeholder: 'Ваша логин',
-  },
-  {
-    id: 'first_name',
-    name: 'first_name',
-    label: 'Имя',
-    placeholder: 'Ваше имя',
-  },
-  {
-    id: 'second_name',
-    name: 'second_name',
-    label: 'Фамилия',
-    placeholder: 'Ваша фамилия',
-  },
-  {
-    id: 'phone',
-    name: 'phone',
-    label: 'Номер телефона',
-    placeholder: '+7(985)126-42-45',
-  },
-  {
-    id: 'password',
-    name: 'password',
-    label: 'Пароль',
-    placeholder: 'Пароль',
-  },
-  {
-    id: 'password-repeat',
-    name: '',
-    label: 'Повторите пароль',
-    placeholder: 'Пароль',
-  },
-];
+const requiredMessage = 'Заполните это поле';
+
+const getRequiredMessage = (required, value) => {
+  if (required && !value) {
+    return requiredMessage;
+  }
+
+  return '';
+};
 
 export default class Component extends Ryabact.Component {
   constructor (context: PropsType = {}) {
     const props: PropsType = {
       ...context,
-      inputs,
       name: context.name,
-      events: context.events,
+      errors: {},
+      params: {},
+      required: {
+        first_name: true,
+        second_name: true,
+        login: true,
+        email: true,
+        phone: true,
+        password: true,
+      },
+      handleInputBlur: (e: Event) => {
+        const name: string = (<HTMLInputElement>e.target).name;
+        const value: string = (<HTMLInputElement>e.target).value;
+        let message: string = '';
+        if (value) {
+          if (name === 'first_name' || name === 'second_name') {
+            message = validation.name(value);
+          } else if (name === 'login') {
+            message = validation.login(value);
+          } else if (name === 'email') {
+            message = validation.email(value);
+          } else if (name === 'phone') {
+            message = validation.phone(value);
+          } else if (name === 'password') {
+            message = validation.password(value);
+
+            if (this.props?.params.password !== this.props?.params.password_repeat) {
+              this.setProps({
+                errors: {
+                  ...(this?.props?.errors || {}),
+                  [name]: message,
+                  password_repeat: 'Значение должно совпадать с полем ПАРОЛЬ',
+                }
+              });
+              return;
+            }
+          } else if (name === 'password_repeat') {
+            if (this.props?.params.password !== this.props?.params.password_repeat) {
+              message = 'Значение должно совпадать с полем ПАРОЛЬ';
+            }
+          }
+        }
+
+
+        this.setProps({
+          errors: {
+            ...this?.props?.errors,
+            [name]: message,
+          }
+        });
+      },
+      handleParamsInput: (e: Event) => {
+        const name: string = (<HTMLInputElement>e.target).name;
+        const value: string = (<HTMLInputElement>e.target).value;
+        this.setProps({
+          params: {
+            ...(this?.props?.params || {}),
+            [name]: value,
+          }
+        });
+      },
+      events: {
+        submit: (e: Event) => {
+          const { required, params } = this.props;
+          const {
+            first_name,
+            second_name,
+            login,
+            email,
+            phone,
+            password,
+          } = params;
+
+          this.setProps({
+            errors: {
+              ...(this?.props?.errors || {}),
+              first_name: getRequiredMessage(required.first_name, first_name) || validation.name(first_name),
+              second_name: getRequiredMessage(required.second_name, second_name) || validation.name(second_name),
+              login: getRequiredMessage(required.login, login) || validation.login(login),
+              email: getRequiredMessage(required.email, email) || validation.email(email),
+              phone: getRequiredMessage(required.phone, phone) || validation.phone(phone),
+              password: getRequiredMessage(required.password, password) || validation.password(password),
+            }
+          });
+
+          e.preventDefault();
+        },
+      }
     };
 
     super({
@@ -68,6 +121,10 @@ export default class Component extends Ryabact.Component {
       },
       containerTemplate: `<div />`,
     });
+  }
+
+  componentDidUpdate (oldProps, newProps) {
+    return !isEqual(oldProps.errors, newProps.errors);
   }
 };
 
