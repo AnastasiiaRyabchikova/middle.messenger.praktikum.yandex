9a8aca9e05d1435a7ed6e2f1f4a14f123fed4c6a
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-12 14:35:13 +0300

    @/validation: add password-repeat

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -3,3 +3,4 @@ export { default as login } from './login';
 export { default as email } from './email';
 export { default as phone } from './phone';
 export { default as password } from './password';
+export { default as passwordRepeat } from './password-repeat';
