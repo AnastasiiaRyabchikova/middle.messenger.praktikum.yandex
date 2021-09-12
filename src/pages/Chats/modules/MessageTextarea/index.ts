0cffb82d5617122366260b485d6620d14da68def
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-12 16:12:40 +0300

    @/pages/Chats/modules/MessageTextarea: fix

--- src/pages/Chats/modules/MessageTextarea/index.ts
+++ src/pages/Chats/modules/MessageTextarea/index.ts
@@ -1,12 +1,61 @@
 import * as Ryabact from 'ryabact';
+import { getRequiredMessage } from '~/src/validation';
+import isEqual from '~/src/utils/is-equal';
+import UITextarea from '~/src/components/UITextarea';
 import { PropsType } from '~/src/types/component';
 import { IconArrow } from '~/src/icons';
 import template from './index.tpl';
+
+const hasErrorsCheck = (errors: { [key: string]: string | null }): boolean => {
+  return Object.values(errors).filter(Boolean).length > 0;
+};
 
 export default class Component extends Ryabact.Component {
   constructor (context: PropsType = {}) {
     const props: PropsType = {
       ...context,
+      name: context.name,
+      errors: {},
+      params: {},
+      required: {
+        text: true,
+      },
+      handleParamsInput: (e: Event) => {
+        const name: string = (<HTMLInputElement>e.target).name;
+        const value: string = (<HTMLInputElement>e.target).value;
+        this.setProps({
+          params: {
+            ...(this?.props?.params || {}),
+            [name]: value,
+          }
+        });
+      },
+      events: {
+        submit: (e: Event) => {
+          e.preventDefault();
+          const { required, params } = this.props;
+          const {
+            text,
+          } = params;
+
+          this.setProps({
+            errors: {
+              ...this.props.errors,
+              text: getRequiredMessage(required.text, text),
+            }
+          });
+
+
+          if (hasErrorsCheck(this.props.errors)) {
+            return;
+          }
+
+          context.events?.submit({
+            text,
+          });
+
+        },
+      }
     };
 
     super({
@@ -15,8 +64,13 @@ export default class Component extends Ryabact.Component {
       template,
       components: {
         IconArrow,
+        UITextarea,
       },
-      containerTemplate: `<span />`,
+      containerTemplate: `<div />`,
     });
+  }
+
+  componentDidUpdate (oldProps, newProps) {
+    return !isEqual(oldProps.errors, newProps.errors);
   }
 };
