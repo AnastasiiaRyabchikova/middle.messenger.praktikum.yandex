c3fe8367c02747bd935442489841f5945bc9b7f9
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-25 16:58:09 +0300

    @/constants/pathnames: fix project links

--- src/constants/pathnames.ts
+++ src/constants/pathnames.ts
@@ -1,12 +1,11 @@
 const pathnames: {
   [key: string]: string
 } = {
-  signIn: 'sign-in',
-  signUp: '',
-  changePassword: 'change-password',
-  chats: 'chats',
-  userForm: 'user-form',
-  error500: 'error-500',
+  signIn: '/sign-in',
+  signUp: '/sign-up',
+  changePassword: '/change-password',
+  chats: '/chats',
+  userForm: '/user-form',
 };
 
 export default pathnames;
