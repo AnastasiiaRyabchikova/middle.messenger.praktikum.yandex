1d221863a7401d3d69b848f5d5cdd25757bd96f4
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-09-25 16:53:51 +0300

    @/routes: fix

--- src/routes.ts
+++ src/routes.ts
@@ -1,24 +1,32 @@
-import { interfaceRyabactComponent } from './types/component';
+import routeSettings from '~/src/Router/interfaces/route-settings';
 import pathnames from '~/src/constants/pathnames';
-
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ChangePassword from './pages/ChangePassword';
 import Chats from './pages/Chats';
 import UserForm from './pages/UserForm';
-import ErrorPage from './pages/Error';
 
-type pageByRoutes = {
-  [pathname: string]: interfaceRyabactComponent,
-};
-
-const routes: pageByRoutes = {
-  [pathnames.signIn]: new SignIn(),
-  [pathnames.signUp]: new SignUp(),
-  [pathnames.changePassword]: new ChangePassword(),
-  [pathnames.chats]: new Chats(),
-  [pathnames.userForm]: new UserForm(),
-  [pathnames.error500]: new ErrorPage({ code: 500 }),
-};
+const routes: Array<routeSettings> = [
+  {
+    pathname: pathnames.signIn,
+    component: SignIn,
+  },
+  {
+    pathname: pathnames.signUp,
+    component: SignUp,
+  },
+  {
+    pathname: pathnames.changePassword,
+    component: ChangePassword,
+  },
+  {
+    pathname: pathnames.chats,
+    component: Chats,
+  },
+  {
+    pathname: pathnames.userForm,
+    component: UserForm,
+  },
+];
 
 export default routes;
