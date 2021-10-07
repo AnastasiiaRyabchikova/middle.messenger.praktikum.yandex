1865deee82f6d9a05651c069e5a7cf6a09de31a9
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-07 08:26:35 +0300

    @/index: fix instance

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import Router from '~/src/modules/Router';
+import Page404 from '~/src/pages/Page404';
 import routes from './routes';
 import './theme/index.css';
 
 const root = document.getElementById('root');
 
-const router = new Router(root);
+if (root) {
+  const router = new Router(root, Page404);
 
-router
-  .use(routes)
-  .start();
+  router
+    .use(routes)
+    .start();
+}
