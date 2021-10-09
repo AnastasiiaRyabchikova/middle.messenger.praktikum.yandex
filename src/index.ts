9eea4226f8df93262308b791f1d68e011ae2e5d2
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-09 16:59:16 +0300

    @: add default routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Router from '~/src/modules/router';
 import Page404 from '~/src/pages/Page404';
-import { routesForUnknownUser } from './routes';
+import Home from './pages/Home';
 import './theme/index.css';
 
 const root = document.getElementById('root');
@@ -9,6 +9,9 @@ if (root) {
   const router = new Router(root, Page404);
 
   router
-    .use(routesForUnknownUser)
+    .use([{
+      pathname: '/',
+      component: Home,
+    }])
     .start();
 }
