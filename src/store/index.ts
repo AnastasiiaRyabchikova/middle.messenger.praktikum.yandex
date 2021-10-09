98c588f52ad711a29d35e9ce116596bbd94ec92d
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-09 10:23:53 +0300

    @/store: init

---
+++ src/store/index.ts
@@ -0,0 +1,29 @@
+import { Component } from 'ryabact';
+import { Store } from '~/src/modules/Store';
+import user from './user';
+
+export const store = new Store({
+  user,
+});
+
+export function connect(
+  stateToProps: (state: any) => any,
+  Element: typeof Component,
+): Component {
+  return class WithStore extends Element {
+    constructor(props: any) {
+      super({ ...props, ...stateToProps(store.getState()) });
+    }
+
+    componentDidMount(props: any) {
+      super.componentDidMount(props);
+
+      store.on('changed', () => {
+        this.setProps({
+          ...stateToProps(store.getState()),
+          ...this.props,
+        });
+      });
+    }
+  };
+}
