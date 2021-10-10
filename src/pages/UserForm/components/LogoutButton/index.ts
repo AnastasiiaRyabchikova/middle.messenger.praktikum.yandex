696aceaf31005a8f617f7309c5e98076738c54dd
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-10 19:37:06 +0300

    @/pages/UserForm/components/LogoutButton: create

---
+++ src/pages/UserForm/components/LogoutButton/index.ts
@@ -0,0 +1,26 @@
+import * as Ryabact from 'ryabact';
+import authControler from '~/src/controlers/auth-controler';
+import { PropsType } from '~/src/types/component';
+import template from './index.tpl';
+
+export default class Component extends Ryabact.Component {
+  constructor(context: PropsType = {}) {
+    const props: PropsType = {
+      ...context,
+      events: {
+        click: async () => {
+          await authControler.logout();
+          window.location.href = '/';
+        },
+      },
+    };
+
+    super({
+      props,
+      name: 'LogoutButton',
+      template,
+      components: {},
+      containerTemplate: '<span />',
+    });
+  }
+};
