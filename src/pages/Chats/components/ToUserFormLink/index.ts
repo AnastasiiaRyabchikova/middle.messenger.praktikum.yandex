2409314a92c5ed89483cd31c7e62b212080288f2
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-10 19:19:01 +0300

    @/pages/Chats/components/ToUserFormLink: create

---
+++ src/pages/Chats/components/ToUserFormLink/index.ts
@@ -0,0 +1,29 @@
+import * as Ryabact from 'ryabact';
+import { withRouter } from 'router';
+import { PropsType } from '~/src/types/component';
+import Avatar from '~/src/components/Avatar';
+import template from './index.tpl';
+
+class ToUserFormLink extends Ryabact.Component {
+  constructor(context: PropsType = {}) {
+    const props: PropsType = {
+      ...context,
+      class: context.class,
+      events: {
+        click: context.evClick,
+      },
+    };
+
+    super({
+      props,
+      name: 'ToUserFormLink',
+      template,
+      components: {
+        Avatar,
+      },
+      containerTemplate: '<span />',
+    });
+  }
+};
+
+export default withRouter(ToUserFormLink);
