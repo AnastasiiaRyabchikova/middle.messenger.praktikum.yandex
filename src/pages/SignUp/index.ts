9e616ab418f2cbc6396eba7bda65d7f34011a584
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-11 08:40:32 +0300

    @/pages/SignUp: add auth

--- src/pages/SignUp/index.ts
+++ src/pages/SignUp/index.ts
@@ -1,22 +1,29 @@
 import * as Ryabact from 'ryabact';
+import { withRouter } from 'router';
 import pathnames from '~/src/constants/pathnames';
 import { PropsType } from '~/src/types/component';
 import RouterLink from '~/src/components/RouterLink';
+import { AuthControler } from '~/src/controlers';
+import { routesForUser } from '~/src/routes';
 import Logo from '../../components/Logo';
 import UIInput from '../../components/UIInput';
 import Button from '../../components/Button';
 import Form from './components/Form';
 import template from './index.tpl';
 import * as styles from './styles.module.css';
+import { connect } from '~/src/store';
 
-export default class Component extends Ryabact.Component {
+class SignUpPage extends Ryabact.Component {
   constructor(context: PropsType = {}) {
     const props: PropsType = {
       ...context,
-      handleFormSubmit: (params: Record<string, unknown>) => {
+      handleFormSubmit: async (params: Record<string, unknown>) => {
         // eslint-disable-next-line no-console
-        console.log(params);
-        this.router.go(pathnames.chats);
+        await AuthControler.signIn(params);
+        if (this.props.user) {
+          this.router.use(routesForUser).start();
+          this.router.go(pathnames.chats);
+        }
       },
     };
 
@@ -34,4 +41,14 @@ export default class Component extends Ryabact.Component {
       containerTemplate: `<div class="${styles.container}" />`,
     });
   }
+  componentDidUpdate() {
+    return false;
+  }
 };
+
+export default withRouter(
+  connect(
+    (state) => ({ user: state.user.profile }),
+    SignUpPage,
+  ),
+);
