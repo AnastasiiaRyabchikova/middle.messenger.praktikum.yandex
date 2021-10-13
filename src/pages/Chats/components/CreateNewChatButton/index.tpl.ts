2f3d9213520cc5e384b61e3d096ec002a91b8ca2
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-13 10:53:00 +0300

    @/pages/Chats/components/CreateNewChatButton: create

---
+++ src/pages/Chats/components/CreateNewChatButton/index.tpl.ts
@@ -0,0 +1,13 @@
+import * as general from '~/src/theme/general.css';
+import * as styles from './styles.module.css';
+
+const template: string = `
+<button
+  type="button"
+  class="${general.buttonReset} ${styles.link}"
+>
+  cоздайте новый
+</button>
+`;
+
+export default template;
---
+++ src/pages/Chats/components/CreateNewChatButton/index.ts
@@ -0,0 +1,19 @@
+import * as Ryabact from 'ryabact';
+import { PropsType } from '~/src/types/component';
+import template from './index.tpl';
+
+export default class Component extends Ryabact.Component {
+  constructor(context: PropsType = {}) {
+    const props: PropsType = {
+      ...context,
+    };
+
+    super({
+      props,
+      name: 'ChatCompanion',
+      template,
+      components: {},
+      containerTemplate: '<span />',
+    });
+  }
+};
