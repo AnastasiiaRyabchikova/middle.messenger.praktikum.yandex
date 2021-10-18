50f91fa1d1513821b0513552c0da30cfde900651
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-18 08:56:16 +0300

    @/icons: fix imports

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as IconArrow } from './Arrow';
 export { default as IconEllipsisVAlt } from './EllipsisVAlt';
 export { default as IconArrowLeft } from './ArrowLeft';
+export { default as IconTimes } from './Times';
