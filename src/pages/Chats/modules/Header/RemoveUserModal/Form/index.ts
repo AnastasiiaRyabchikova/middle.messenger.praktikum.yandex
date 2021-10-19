debc36de3da10d509b7faaa387d29cf6c743cda2
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-19 17:21:47 +0300

    @/pages/Chats/modules/Header/RemoveUserModal/Form: create

---
+++ src/pages/Chats/modules/Header/RemoveUserModal/Form/index.ts
@@ -0,0 +1,75 @@
+import * as Ryabact from 'ryabact';
+import { PropsType } from '~/src/types/component';
+import isEqual from '~/src/utils/is-equal';
+import { getRequiredMessage } from '~/src/validation';
+import UIInput from '~/src/components/UIInput';
+import Button from '~/src/components/Button';
+import template from './index.tpl';
+
+const hasErrorsCheck = (errors: { [key: string]: string | null }): boolean => (
+  Object.values(errors).filter(Boolean).length > 0
+);
+
+export default class Component extends Ryabact.Component {
+  constructor(context: PropsType = {}) {
+    const props: PropsType = {
+      ...context,
+      errors: {},
+      params: {},
+      required: {
+        ids: true,
+      },
+      handleParamsInput: (e: Event) => {
+        const { name, value }: valuesType = (<HTMLInputElement>e.target);
+
+        this.setProps({
+          params: {
+            ...this?.props?.params,
+            [name]: value,
+          },
+        });
+      },
+      events: {
+        submit: (e: Event) => {
+          e.preventDefault();
+          const { required = {}, params = {} } = this.props;
+          const {
+            ids,
+          } = params;
+
+          this.setProps({
+            errors: {
+              ...this.props.errors,
+              ids: getRequiredMessage(required.ids, ids),
+            },
+          });
+
+          if (hasErrorsCheck(this.props.errors || {})) {
+            return;
+          }
+
+          const users = ids
+            .split(',')
+            .map(Number);
+
+          context.events?.submit(users);
+        },
+      },
+    };
+
+    super({
+      props,
+      name: 'CreateNewChatModalForm',
+      template,
+      components: {
+        UIInput,
+        Button,
+      },
+      containerTemplate: '<div />',
+    });
+  }
+
+  componentDidUpdate(oldProps: PropsType, newProps: PropsType): boolean {
+    return !isEqual(oldProps.errors, newProps.errors);
+  }
+};
