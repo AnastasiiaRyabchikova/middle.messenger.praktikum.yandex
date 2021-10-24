ab11aeb9c7257016780e89d395a010ef4ab1a779
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-24 18:50:51 +0300

    @/utils/base-api: add HTTP init

--- src/utils/base-api/index.ts
+++ src/utils/base-api/index.ts
@@ -3,7 +3,7 @@ import HTTPTransport from '../../modules/HTTPTransport';
 export default class BaseApi {
   http: HTTPTransport;
 
-  constructor(http: HTTPTransport) {
-    this.http = http;
+  constructor(endPoint: string) {
+    this.http = new HTTPTransport(endPoint);
   }
 };
