ae58ec93e9bd6894ba7c7613682afa11683195b8
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-28 13:45:44 +0300

    @/components/Avatar: fix imports

--- src/components/Avatar/index.ts
+++ src/components/Avatar/index.ts
@@ -2,7 +2,7 @@ import * as Ryabact from 'ryabact';
 import cx from 'classnames';
 import { PropsType } from '../../types/component';
 import template from './index.tpl';
-import * as styles from './styles.module.css';
+import styles from './styles.module.css';
 
 export default class Component extends Ryabact.Component {
   constructor(context: PropsType = {}) {
