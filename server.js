2d2b3db55f4acf64d649553fcba4bd3cd2796df6
Ryabchikova Anastasia <aryabchikova@ubic.tech>
2021-10-29 09:39:38 +0300

    @/server.js: fix

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./'));
+app.use('/', express.static('./dist'));
+app.use('/sign-up', express.static('./dist'));
+app.use('/change-password', express.static('./dist'));
+app.use('/user-form', express.static('./dist'));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
